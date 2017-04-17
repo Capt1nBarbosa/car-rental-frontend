4c4ebaa6c9fc4874ecb5e8c633d5a789e3477072
acpbm <acpbm0@gmail.com>
2017-04-17 02:12:43 -0400

    Declare variables for search method

--- public/scripts/services/vehicleService.js
+++ public/scripts/services/vehicleService.js
@@ -2,8 +2,8 @@ vehicleService.$inject = ['$http'];
 
 function vehicleService($http) {
   return {
-    getAllVehicles: getAllVehicles(),
-    search: search()
+    getAllVehicles: getAllVehicles,
+    search: search
   };
 
   function getAllVehicles(){
@@ -18,6 +18,10 @@ function vehicleService($http) {
   }
 
   function search(location){
+      console.log('hit serach method in vehicle service');
+      var data = [];
+      var vehicles = [];
+      var units = [];
     return $http.get(`http://localhost:3000/vehicles/search?location=${location}`)
       .then(function(response){
         console.log(response);
