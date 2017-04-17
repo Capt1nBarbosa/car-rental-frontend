575fcfb5a93b429921a2c5f81c323c08e4633597
acpbm <acpbm0@gmail.com>
2017-04-17 02:10:01 -0400

    Refactor search method into browse method, add focus method

--- public/scripts/controllers/vehicleController.js
+++ public/scripts/controllers/vehicleController.js
@@ -1,35 +1,38 @@
-VehicleController.$inject = ['VehicleService', '$state'];
-function VehicleController(vehicleService, $state){
+VehicleController.$inject = ['vehicleService', '$state', '$scope', '$localStorage'];
+
+function VehicleController(vehicleService, $state, $scope, $localStorage){
   //capture variable to avoid problems with context of 'this' in functons within the controller functon.
   //vm stands for view model
   var vm = this;
   //bindable members (public data and methods that can be used in the interface/view)
   vm.allVehicles = [];
+  vm.browse = browse;
   vm.getAllVehicles = getAllVehicles;
+  vm.location = '';
   vm.resultVehicles = [];
-  vm.search = search;
 
+  browse();
+  // focus();
 
-  //get all vehicles
   function getAllVehicles(){
     vehicleService.getAllVehicles()
       .then(function(response){
         console.log(response);
-        vm.allVehicles = response;
+        vm.allVehicles = response.data;
       });
   }
 
-  //search for vehicles in specified location
-  function search(){
-    //get data from form
-    var location = $("#location").val();
-    //make ajax call to get search results
-    vehicleService.search(location)
-      .then(function(response){
-        console.log(response);
-        vm.vehicles = response;
-        $state.go('browse');
-      });
+  function browse(){
+    console.log('hit serach method in vehicle controller');
+    vm.location = $localStorage.location;
+    vm.resultVehicles = $localStorage.resultVehicles;
 
+  }
+
+  function focus(){
+    $("#location").css("border-bottom-color", "#ffffff");
+    $("#location h5").css("color", "#ffffff");
+    $("#select-vehicle").css("border-bottom-color", "#b388ff");
+    $("#select-vehicle h5").css("color", "#b388ff");
   }
 }
