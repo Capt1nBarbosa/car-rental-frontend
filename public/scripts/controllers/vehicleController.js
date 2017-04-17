327921ee5a05568270ab940a3425f222909a2547
acpbm <acpbm0@gmail.com>
2017-04-17 17:11:03 -0400

    Clean up logs and remove focus function

--- public/scripts/controllers/vehicleController.js
+++ public/scripts/controllers/vehicleController.js
@@ -1,10 +1,9 @@
 VehicleController.$inject = ['vehicleService', '$state', '$scope', '$localStorage'];
 
-function VehicleController(vehicleService, $state, $scope, $localStorage){
+function VehicleController(vehicleService, $state, $localStorage){
   //capture variable to avoid problems with context of 'this' in functons within the controller functon.
-  //vm stands for view model
   var vm = this;
-  //bindable members (public data and methods that can be used in the interface/view)
+
   vm.allVehicles = [];
   vm.browse = browse;
   vm.getAllVehicles = getAllVehicles;
@@ -12,27 +11,16 @@ function VehicleController(vehicleService, $state, $scope, $localStorage){
   vm.resultVehicles = [];
 
   browse();
-  // focus();
 
   function getAllVehicles(){
     vehicleService.getAllVehicles()
       .then(function(response){
-        console.log(response);
         vm.allVehicles = response.data;
       });
   }
 
   function browse(){
-    console.log('hit serach method in vehicle controller');
     vm.location = $localStorage.location;
     vm.resultVehicles = $localStorage.resultVehicles;
-
-  }
-
-  function focus(){
-    $("#location").css("border-bottom-color", "#ffffff");
-    $("#location h5").css("color", "#ffffff");
-    $("#select-vehicle").css("border-bottom-color", "#b388ff");
-    $("#select-vehicle h5").css("color", "#b388ff");
   }
 }
