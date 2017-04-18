d4b26c06e53e9d984aeac329b555fce241f54ba2
acpbm <acpbm0@gmail.com>
2017-04-17 22:27:16 -0400

    Create user controller

---
+++ public/scripts/controllers/userController.js
@@ -0,0 +1,19 @@
+UserController.$inject = [];
+function UserController(){
+  var vm = this;
+
+  vm.name = '';
+  vm.email = '';
+  vm.dob = '';
+  vm.phoneNumber = '';
+  vm.insurance = '';
+  vm.address = {};
+  vm.licence = '';
+
+  function add(user){
+
+  }
+
+  function remove(){}
+
+}
