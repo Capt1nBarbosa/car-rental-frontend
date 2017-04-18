9cfbd703623214224552cc588ddd265cb1e62b23
acpbm <acpbm0@gmail.com>
2017-04-18 09:24:16 -0400

    Add helper functions

--- public/scripts/controllers/mainController.js
+++ public/scripts/controllers/mainController.js
@@ -2,43 +2,86 @@ MainController.$inject = ['$scope', '$state', '$localStorage', 'vehicleService']
 
 function MainController($scope, $state, $localStorage, vehicleService){
 
+  // NOTE: refactor this controller into smaller services to clean up and make more readable
+
   var vm = this;
-  vm.search = search;
+  vm.currentUser = null;
+  vm.isLoggedIn = isLoggedIn;
   vm.location = '';
+  vm.reservationStarted = false;
+  vm.reservationComplete = false;
+  vm.search = search;
+  vm.pickUp = '';
+  vm.dropOff = '';
+  vm.daysTotal = '';
+  vm.vehicle = {};
 
   $scope.$on('userLoggedIn', function(event, data){
     //add user info to localStorage
-    $localStorage.name = data.name;
-    $localStorage.email = data.email;
-    $localStorage.userId = data.id;
+    $localStorage.currentUser = data;
     //add user info to controller data
-    vm.currentUser = data;
+    vm.currentUser = $localStorage.currentUser;
   });
 
   $scope.$on('userLoggedOut', function(event, data) {
     //remove userinfo from localStorage
-    delete $localStorage.name;
-    delete $localStorage.email;
-    delete $localStorage.userId;
+    delete $localStorage.currentUser;
     //remove user info from controller data
-    vm.currentUser = null;
+    vm.currentUser = $localStorage.currentUser;
   });
 
   function getLocation(){
     $localStorage.location =
     $("#location-value").val();
-
     vm.location = $localStorage.location;
+  }
+
+  function getDates(){
+    $localStorage.pickup = $("#pick-up-date").val();
+    $localStorage.dropOff = $("#return-date").val();
+    vm.pickUp = $localStorage.pickup;
+    vm.dropOff = $localStorage.dropOff;
+  }
+
+  function getSelectedVehicle(vehicle){
+    $localStorage.vehicle = vehicle;
+    vm.vehicle = $localStorage.vehicle;
+  }
+
+  function calculateTotalDays(start, end){
+    var date1 = new Date(start);
+    var date2 = new Date(end);
+    var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    $localStorage.daysTotal = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    vm.daysTotal = $localStorage.daysTotal;
+  }
+
+  function isLoggedIn(vehicle){
+    getSelectedVehicle(vehicle);
+    if(!vm.currentUser){
+      // NOTE: make reservationStarted into a method
+      $localStorage.reservationStarted = true;
+      vm.reservationStarted = $localStorage.reservationStarted;
+      $localStorage.reservationComplete = false;
+      vm.reservationComplete = $localStorage.reservationComplete;
+      $state.go('login');
+    }else{
+      $state.go('reserve');
+    }
   }
 
   function search(){
     getLocation();
-    vehicleService.search($localStorage.location)
+    getDates();
+    calculateTotalDays(vm.pickUp, vm.dropOff);
+    vehicleService.search(vm.location)
       .then(function(response){
         console.log(response);
         $localStorage.resultVehicles = response.data;
         console.log($localStorage.resultVehicles);
-        $state.go('browse', {searchByLocation: $localStorage.location}, {reload: true});
+        $state.go('browse', {searchByLocation: $localStorage.location});
       });
   }
+
+
 }
