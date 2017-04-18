1728bb55a0ebbf83cc5957e7a1ce37bb593629c4
acpbm <acpbm0@gmail.com>
2017-04-17 22:24:37 -0400

    Create reservation controller

---
+++ public/scripts/controllers/reservationController.js
@@ -0,0 +1,19 @@
+ReservationController.$inject = [];
+function ReservationController(){
+  var vm = this;
+
+  vm.pickUp = '';
+  vm.dropOff = '';
+  vm.pickUpLocation = '';
+  vm.confirmationNum = '';
+  vm.unitId = '';
+
+  function make(vehicle){}
+
+  function cancel(){}
+
+  function change(){}
+
+  function calculateTotalPrice(){}
+
+}
