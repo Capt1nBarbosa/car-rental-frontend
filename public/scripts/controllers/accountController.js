d72687c9992b3703a3c8c1bbdfe36a2b96a92e48
acpbm <acpbm0@gmail.com>
2017-04-17 22:23:40 -0400

    Create account controller

---
+++ public/scripts/controllers/accountController.js
@@ -0,0 +1,15 @@
+AccountController.$inject = [];
+function AccountController(){
+  var vm = this;
+
+  function edit(){}
+
+  function checkType(){}
+
+  function close(){}
+
+  function getReservations(){}
+
+  function open(){}
+
+}
