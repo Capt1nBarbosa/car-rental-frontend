1ea8f6b5412c826e24304b2b0304017ce34d523a
acpbm <acpbm0@gmail.com>
2017-04-18 09:25:17 -0400

    Create reservation controller

--- public/scripts/controllers/reservationController.js
+++ public/scripts/controllers/reservationController.js
@@ -1,19 +1,54 @@
-ReservationController.$inject = [];
-function ReservationController(){
+ReservationController.$inject = ['$localStorage', 'vehicleService', 'unitService', '$scope', '$state'];
+function ReservationController($localStorage, vehicleService, unitService, $scope, $state){
   var vm = this;
 
   vm.pickUp = '';
   vm.dropOff = '';
   vm.pickUpLocation = '';
   vm.confirmationNum = '';
-  vm.unitId = '';
+  vm.total = '';
+  vm.calculateTotalPrice = calculateTotalPrice;
+  vm.make = make;
 
-  function make(vehicle){}
+  vm.unit = '';
 
-  function cancel(){}
+  function make(vehicleId, location){
+    // NOTE:  migth need to merge with vehicle method reserve()
+    vehicleService.checkAvailability(vehicleId, location)
+      .then(function(response){
+        console.log(response);
+        $localStorage.unit = response[0];
+        $localStorage.unit.isAvailable = false;
+        vm.unit = $localStorage.unit;
+        console.log(vm.unit);
+        return vm.unit;
+      })
+      .then(function(unit){
+        vehicleService.reserve(unit)
+          .then(function(response){
+            console.log(response);
+            $state.go('complete');
+          })
+          .catch(function(error){
+            console.log(error);
+          });
+      })
+      .catch(function(error){
+        console.log(error);
+      });
+  }
+
+  function cancel(){
+    //check that date is not passed pickip Date
+    //update unit to isAvaliable = true
+    //update reservation to isOpen = false
+  }
 
   function change(){}
 
-  function calculateTotalPrice(){}
+  function calculateTotalPrice(rate){
+    vm.total = $localStorage.daysTotal * rate;
+    return vm.total;
+  }
 
 }
