830d8a3c09f43d1c2c0d04c775df7cd4b51961ea
acpbm <acpbm0@gmail.com>
2017-04-18 09:27:54 -0400

    Add checkAvailability and reserve functions

--- public/scripts/services/vehicleService.js
+++ public/scripts/services/vehicleService.js
@@ -1,13 +1,43 @@
-vehicleService.$inject = ['$http'];
+vehicleService.$inject = ['$http', 'unitService', '$localStorage'];
 
-function vehicleService($http) {
+function vehicleService($http, unitService, $localStorage) {
+  var server = 'http://localhost:3000';
   return {
+    checkAvailability: checkAvailability,
     getAllVehicles: getAllVehicles,
+    reserve: reserve,
     search: search
   };
 
+  function checkAvailability(vehicleId, location) {
+    console.log(vehicleId);
+    console.log(location);
+    return $http.get(`${server}/vehicles/${vehicleId}?location=${location}`)
+      .then(function(response){
+        console.log(response);
+        return response.data.data;
+      })
+      .catch(function(error){
+        console.log(error);
+        return error;
+      });
+  }
+
+  function reserve(unit){
+    return unitService.update(unit, $localStorage.currentUser.id)
+      .then(function(response){
+        console.log(response);
+        $localStorage.reservationComplete = true;
+        return response;
+      })
+      .catch(function(error){
+        console.log(error);
+        return error;
+      });
+  }
+
   function getAllVehicles(){
-    return $http.get('http://localhost:3000/vehicles')
+    return $http.get(`${server}/vehicles`)
       .then(function(response){
         console.log(response);
         return response.data;
@@ -22,7 +52,7 @@ function vehicleService($http) {
       var data = [];
       var vehicles = [];
       var units = [];
-    return $http.get(`http://localhost:3000/vehicles/search?location=${location}`)
+    return $http.get(`${server}/vehicles/search?location=${location}`)
       .then(function(response){
         console.log(response);
         return response.data;
