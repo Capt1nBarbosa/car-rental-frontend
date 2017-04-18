97561a1876c2d33491184ec417fb1951defde1bc
acpbm <acpbm0@gmail.com>
2017-04-18 09:22:02 -0400

    Add unit service and notes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,32 @@ angular.module('reserve-a-car', ['ui.router', 'ngStorage'])
   .factory('authInterceptor', authInterceptor)
   .factory('authTokenService', authTokenService)
   .factory('vehicleService', vehicleService)
+  .factory('unitService', unitService)
   .controller('AuthController', AuthController)
   .controller('VehicleController', VehicleController)
   .controller('ReservationController', ReservationController)
   .controller('MainController', MainController);
+
+// NOTE: if times allows refactor all services and controller into ES6 classes
+
+// NOTE: look into jasmine or mocha for unit testing
+
+// NOTE: add pesimistic locking or find_with_lock to backend when checking availability - NOT OPTIONAL
+
+// NOTE: fix bug with browse.html sorting checkboxes
+
+// NOTE: fix bug when refreshig the page signs user out
+
+// NOTE: fix bug page refresh on reservation page logs out user and losses data, use $window.history to resolve
+
+// NOTE: fix bug on logout search form datepicker input is not cleared
+
+// NOTE: make diagrams showing sequence diagram, class model, and code snippets for each use case
+
+// NOTE: add comments through code to explain use and purpose
+
+// NOTE: fix review vehicle table size
+
+// NOTE: add better error handler for promises
+
+// NOTE: look for random reservation number generator, if not use ruby faker gem
